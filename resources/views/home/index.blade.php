18b42c6334273e7b9587d2ba377d5ac378f54399
chenyirou <annie851216@gmail.com>
2017-10-18 17:47:13 +0800

    編輯 index.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,10 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-<h1>Home</h1>
-</body>
-</html>
+@extends('layouts.master')
+@section('title','Home')
+@section('content')
+    <h1>Home</h1>
+    <p>Thisis my body content.</p>
+@endsection
