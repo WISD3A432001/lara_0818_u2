dc6264090186f308f260e7a210ea03a6c4610930
chenyirou <annie851216@gmail.com>
2017-10-18 17:25:34 +0800

    1-2 設定從 app/Http/routes.php回傳這個頁面

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
+});
+*/
+
+//練習1-2
+Route::get('home',function(){
+    return view('home');
 });
