c054b4720c849de864e8e5c1e9cd27c431013f3f
chenyirou <annie851216@gmail.com>
2017-10-18 18:13:52 +0800

    修改hello/index.blade.php

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,7 +1,5 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 2017/10/18
- * Time: 下午 06:11
- */
+@extends('layouts.master')
+@section('title','Hello')
+@section('content')
+    <h1>This is{{$name}}'shome.</h1>
+@endsection
