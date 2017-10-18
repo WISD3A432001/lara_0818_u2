8cee42451c3fb4b15d45cfd064d5c088e996fe37
chenyirou <annie851216@gmail.com>
2017-10-18 18:18:59 +0800

    從 HelloController 將參數傳遞至 view

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,7 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+//練習3-4
+
 class HelloController extends Controller
 {
-    //
+    public function index($name='Everybody')
+    {
+        $data=['name'=>$name];
+        return view('hello.index',$data);
+    }
+
 }
